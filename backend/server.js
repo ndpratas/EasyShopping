7d4596850c6c7ca8a6be8d9526ff870a8e13b0a4
Jose Cardoso () <jose.cardoso-ext@man.eu>
2019-03-21 21:34:39 +0000

    search

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
+const stringSimilarity = require('string-similarity');
+const fs = require('fs');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -8,11 +10,21 @@ app.get('/tag-recognition', async(req, res) => {
     const result = await extractTagsFromImage();
     const tags = result.split("\n");
     res.send(JSON.stringify({productTags:tags.filter(v=>v!='')}));
-})
+});
 
 app.get('/search',(req, res) => {
-    console.log(req.query.tag);
-    res.send(req.query.tag);
+    const tags = req.query.tag;
+    const productList = JSON.parse(fs.readFileSync('products.json','utf8'));
+    let result = [];
+    productList.forEach(function(product){
+        const similarityQF = stringSimilarity.compareTwoStrings(tags,product.productTags.toString());
+        console.log(similarityQF);
+        //TODO: review this alghoritm if the number of tags is diferent of tag query parameters
+        if(similarityQF>0.1) {
+            result.push(product);
+        }
+    });
+    res.send(result);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
@@ -20,7 +32,8 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 async function extractTagsFromImage() {
     const vision = require('@google-cloud/vision');
     const client = new vision.ImageAnnotatorClient();
-    const fileName = 'milk.jpeg';
+    const fileName = 'http://www.hipersuper.pt/wp-content/uploads/2018/01/nsl-498x1024.jpg'
+    //const fileName = 'milk.jpeg';
     const [result] = await client.textDetection(fileName);
     const detections = result.textAnnotations;
     return detections[0].description;
