7d1cd8b74b4d7a4ad4a3102636789cc8bcf7d2d6
Brites, Nuno (EXTERN: FIEE) <nuno.brites-ext@man.eu>
2019-03-22 22:41:54 +0000

    Adds mic and sound wave logic

--- frontend/app/src/home-page/index.js
+++ frontend/app/src/home-page/index.js
@@ -14,10 +14,10 @@ const homePage = ({onResultsFound, history}) => {
   return (
     <div id="home-page">
       <section id="main-area">
-        <img id="user-thumbnail" src="https://static1.squarespace.com/static/56262570e4b0504d6aeebeb1/563753a7e4b09ebbd96faa3c/563755fbe4b02ed46f8b80dd/1446467070961/Folio-Ring-flash-old-man.jpg" />
-        <SpeechRecognition onResultsFound={homePageOnResultsFound} />
+        <img id="user-thumbnail" src="https://static1.squarespace.com/static/56262570e4b0504d6aeebeb1/563753a7e4b09ebbd96faa3c/563755fbe4b02ed46f8b80dd/1446467070961/Folio-Ring-flash-old-man.jpg" />        
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
         <h2 style={{ margin: 0, textAlign: "center" }}>Welcome back Robert. What do you need today?</h2>
+        <SpeechRecognition onResultsFound={homePageOnResultsFound} />
       </section>
 
       <section id="options-area">
--- frontend/app/src/speech-to-text/dictaphone.js
+++ frontend/app/src/speech-to-text/dictaphone.js
@@ -4,6 +4,8 @@ import Speech from 'react-speech';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import $ from "jquery";
+import audioWave from './AudioVisualizer.gif';
+import mic from './mic.png';
 
 const propTypes = {
   transcript: PropTypes.string,
@@ -61,7 +63,7 @@ class Dictaphone extends Component {
   }
 
   render() {
-    const { finalTranscript, resetTranscript, browserSupportsSpeechRecognition, startListening } = this.props
+    const { finalTranscript, transcript, browserSupportsSpeechRecognition, startListening, listening } = this.props
     const { numOfProducts, latestText } = this.state;
 
     if (!browserSupportsSpeechRecognition) {
@@ -72,12 +74,16 @@ class Dictaphone extends Component {
 
     return (
       <div>
-        <div>
+        {/* <div>
           <button onClick={startListening}>Start</button>
           <button onClick={resetTranscript}>Reset</button>
+        </div> */}
+        <div>
+          <img alt="mic" src={mic} style={{ display: listening ? 'none' : ' block'}} onClick={startListening}/>
+          <img alt="AudioVisualizer" src={audioWave} style={{ width: '300px', height: '200px', display: listening ? 'block' : ' none' }} />
+          <div style={{ display: (listening && transcript !== '') ? 'block' : 'none'}}>Searching...</div>
         </div>
-        <div>Searching...</div>
-        <div>{finalTranscript}</div>
+        {/* <div>{finalTranscript}</div> */}
         {this.postSpeechText(finalTranscript)}
         <div>
           <Speech
