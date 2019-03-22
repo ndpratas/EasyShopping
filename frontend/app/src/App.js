fbfa03800c0ca227d45ab103bfb33d35fc442368
Brites, Nuno (EXTERN: FIEE) <nuno.brites-ext@man.eu>
2019-03-22 12:43:23 +0000

    - Adds mockPostSpeech call - Sets speech detection to start on button click

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div style={{backgroundColor: "white", display: "flex", flexDirection: "column", height: "100%"}}>
         <Navbar />
         <HomePage />
-        {/* <SpeechRecognition /> */}
+        <SpeechRecognition />
       </div>
     );
   }
