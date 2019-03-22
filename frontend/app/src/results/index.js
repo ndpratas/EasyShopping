d99157a4722d0932afc889c876453e23239380bf
Oliveira, Alexandra  (EXTERN: FIEED) <alexandra.oliveira-ext@man.eu>
2019-03-22 23:16:39 +0000

    Refine

--- frontend/app/src/results/index.js
+++ frontend/app/src/results/index.js
@@ -7,7 +7,7 @@ const renderResults = (results) => {
         <img src={result.thumbnail} style={{width: "200px", height: "200px"}}></img>
         <h1>{`${result.price}€`}</h1>
         <p>{result.name}</p>
-        <button><i class="fas fa-shopping-cart"></i><span> </span>Add to cart</button>
+        <button className="shop-button"><i class="fas fa-shopping-cart"></i><span> </span>Add to cart</button>
       </section>
     )
   );
@@ -16,7 +16,7 @@ const renderResults = (results) => {
 const resultsPage = ({results}) => (
   <div style={{padding: "20px 40px"}}>
     <h3 style={{marginLeft: "40px", paddingBottom: "20px"}}>{`${results.length} result(s) found`}</h3>
-    <div style={{display: "flex", justifyContent: "space-around", flexWrap: "wrap"}}>
+    <div style={{display: "flex", justifyContent: "space-around"}}>
       {renderResults(results)}
     </div>
   </div>
